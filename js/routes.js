b616d51546367cede8a8ffa776edc4ba4a26f9c2
Csaba Fazekas <csabafazekasster@gmail.com>
2016-09-24 23:31:54 +0200

    [ADD] why it works page

--- js/routes.js
+++ js/routes.js
@@ -6,6 +6,7 @@
         .when('/chords', { templateUrl: '/views/templates/lesson_templates/chords.html', controller: 'lessonCtrl', needAuth: true })
         .when('/login', { templateUrl: '/views/templates/page_templates/login.html' })
         .when('/pricing', { templateUrl: '/views/templates/page_templates/pricing.html' })
+        .when('/why_it_works', { templateUrl: '/views/templates/page_templates/why_it_works.html' })
         .when('/chords/:id', { templateUrl: '/views/templates/lesson_templates/lesson_detail.html', controller: 'lessonCtrl', needAuth: true })
         .when('intermediate/scales/:id', { templateUrl: '/views/templates/lesson_templates/lesson_detail.html', controller: 'lessonCtrl' })
         .when('intermediate/technique/:id', { templateUrl: '/views/templates/lesson_templates/lesson_detail.html', controller: 'lessonCtrl' })
